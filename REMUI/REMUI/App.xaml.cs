e8066f29b653437fc093deb90de98623ea7f92d5
maiiaKey <mayak29052003@gmail.com>
2021-07-29 22:43:44 +0200

    Created Orders page, removed MainPage

--- REMUI/REMUI/App.xaml.cs
+++ REMUI/REMUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using REMUI.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace REMUI
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Orders();
         }
 
         protected override void OnStart()
--- REMUI/REMUI/Pages/Orders.xaml.cs
+++ REMUI/REMUI/Pages/Orders.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace REMUI
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace REMUI.Pages
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Orders : ContentPage
     {
-        public MainPage()
+        public Orders()
         {
             InitializeComponent();
         }
